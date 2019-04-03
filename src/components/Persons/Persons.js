ad9548e024ae2effe94037a521a77e90f2084074
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-04-03 17:22:58 -0300

    after convert person and persons to function based components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-    console.log('[Persons.js] render');
-    return (
-        props.persons.map((person, index) => {
-            return (
-                <Person 
-                    key={person.id}
-                    name={person.name} 
-                    age={person.age}
-                    click={() => props.clicked(index)}
-                    change={(event) => props.changed(event, person.id)}>
-                </Person>
-            )
-        })
-    )
+class Persons extends Component {
+
+    render() {
+        console.log('[Persons.js] render');
+        return (
+            this.props.persons.map((person, index) => {
+                return (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age}
+                        click={() => this.props.clicked(index)}
+                        change={(event) => this.props.changed(event, person.id)}>
+                    </Person>
+                )
+            })
+        )
+    }
 }
 
-export default persons; 
+export default Persons; 
